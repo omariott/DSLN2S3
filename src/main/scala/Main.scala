270166aec2ca005a00754617f43e2f271ecac91b
Octave Mariotti <octave.mariotti@ens-lyon.fr>
2017-04-03 16:39:11 +0200

    Finished declarative syntax

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,74 +3,57 @@ import fr.univ_lille.cristal.emeraude.n2s3.core.models.properties.{MembraneLeakT
 import fr.univ_lille.cristal.emeraude.n2s3.features.builder.connection.types.FullConnection
 import fr.univ_lille.cristal.emeraude.n2s3.features.io.input.N2S3InputStreamCombinators._
 import fr.univ_lille.cristal.emeraude.n2s3.features.io.input.{MnistFileInputStream, _}
-import fr.univ_lille.cristal.emeraude.n2s3.models.bio.{InhibitorConnection, LIFNeuron, Synapse}
+import fr.univ_lille.cristal.emeraude.n2s3.features.io.report.BenchmarkMonitorRef
+import fr.univ_lille.cristal.emeraude.n2s3.models.qbg.{QBGInhibitorConnection, QBGNeuron, QBGNeuronConnectionWithNegative}
 import fr.univ_lille.cristal.emeraude.n2s3.support.UnitCast._
 import squants.electro.ElectricPotentialConversions.ElectricPotentialConversions
-import squants.time.Hertz
-
-import scala.util.Random
 
 object Main extends App {
+
+  val MNISTTrainingSet = "/home/omariott/Documents/M2/Stage/MNIST/train-images.idx3-ubyte"
+  val MNISTTrainingLabels = "/home/omariott/Documents/M2/Stage/MNIST/train-labels.idx1-ubyte"
+  val MNISTTestSet = "/home/omariott/Documents/M2/Stage/MNIST/t10k-images.idx3-ubyte"
+  val MNISTTestLabels = "/home/omariott/Documents/M2/Stage/MNIST/t10k-labels.idx1-ubyte"
 
   /**
     * Declarative part: define the network
     */
   implicit val network = new Network[SampleUnitInput, SampleInput]
+
+  network hasInput MnistEntry() >> SampleToSpikeTrainConverter(0, 50, 35 MilliSecond, 25 MilliSecond)
+
   network withInputNeuronGroup "input"
-  network withNeuronGroup "hidden" ofSize 40 modeling LIFNeuron withParameters (
+  network withNeuronGroup ("hidden" ofSize 40 modeling QBGNeuron withParameters (
     MembraneThresholdPotential -> 1.millivolts,
     MembraneLeakTime -> 20.MilliSecond
-  )
-  network withNeuronGroup "output" ofSize 10 modeling LIFNeuron withParameters (
+  ))
+  network withNeuronGroup ("output" ofSize 10 modeling QBGNeuron withParameters (
     (MembraneThresholdPotential, 1 millivolts)
-    )
+    ))
 
-  "input" connectTo "hidden" using new FullConnection(() => new Synapse(Random.nextFloat * 0.1f))
-  "hidden" connectTo "output" using new FullConnection(() => new Synapse(Random.nextFloat * 0.1f))
+  "input" connectTo "hidden" using new FullConnection(() => new QBGNeuronConnectionWithNegative)
+  "hidden" connectTo "output" using new FullConnection(() => new QBGNeuronConnectionWithNegative)
 
-  "hidden" connectTo "hidden" using new FullConnection(() => new InhibitorConnection)
-  "output" connectTo "output" using new FullConnection(() => new InhibitorConnection)
+  "hidden" connectTo "hidden" using new FullConnection(() => new QBGInhibitorConnection)
+  "output" connectTo "output" using new FullConnection(() => new QBGInhibitorConnection)
 
+  val graph = observeConnectionsBetween("input", "output")
 
   /**
     * Imperative part: start the network
     */
-  val inputStream = MnistEntry() >> StreamOversampling(1, 1) >> SampleToSpikeTrainConverter(0, 50, 35 MilliSecond, 25 MilliSecond) >> InputNoiseGenerator(Hertz(50))
-  network.hasInput(inputStream, new MnistFileInputStream("/home/omariott/Documents/M2/Stage/MNIST/train-images.idx3-ubyte", "/home/omariott/Documents/M2/Stage/MNIST/train-labels.idx1-ubyte").shuffle() take 10)
-
-  registerBenchmarkMonitorOn("hidden")
-
-  makeSynapsesWeightGraph("input", "hidden")
-
-  val n2s3 = network.toN2S3
-
-  //Should be moved up, to the declarative part
-  //n2s3.addNetworkObserver(new SynapsesWeightGraphBuilderRef("input", "hidden"))
-  //n2s3.addNetworkObserver(new SynapsesWeightGraphBuilderRef("input", "output"))
-
-  n2s3.runAndWait()
+  network trainOn new MnistFileInputStream(MNISTTrainingSet, MNISTTrainingLabels).shuffle()//.take(1000)
 
   /**
     * Imperative part: Prepare the network for testing
     */
-  fixWeights("hidden")
-  fixWeights("output")
-
-  /**
-    * Should be declarative: Declare testing set
-    */
-  inputStream.clean()
-  inputStream.append(new MnistFileInputStream("/home/omariott/Documents/M2/Stage/MNIST/t10k-images.idx3-ubyte", "/home/omariott/Documents/M2/Stage/MNIST/t10k-labels.idx1-ubyte") take 10)
-
-  //val benchmarkMonitor = network addObserver new BenchmarkMonitorRef("hidden")
-  //val benchmarkMonitor = n2s3.createBenchmarkMonitor("hidden")
+  val benchmarkMonitor = network addObserver new BenchmarkMonitorRef("hidden")
 
   /**
     * Imperative part: start the network for testing
     */
-  n2s3.runAndWait()
+  network testOn new MnistFileInputStream(MNISTTestSet, MNISTTestLabels)//.take(200)
 
-  val benchmarkMonitor = getBenchmarkObserverOn("hidden")
   val result = benchmarkMonitor.getResult
   println("res hidden = " + result.evaluationByMaxSpiking.score + "/" + result.inputNumber)
   benchmarkMonitor.exportToHtmlView("res.html")
--- src/main/scala/dsl/NetworkImplicits.scala
+++ src/main/scala/dsl/NetworkImplicits.scala
@@ -4,13 +4,14 @@
 
 package dsl
 
+import java.util.NoSuchElementException
+
 import fr.univ_lille.cristal.emeraude.n2s3.core.{NeuronModel, Property}
 import fr.univ_lille.cristal.emeraude.n2s3.features.builder.connection.ConnectionPolicy
-import fr.univ_lille.cristal.emeraude.n2s3.features.builder.connection.types.FullConnection
-import fr.univ_lille.cristal.emeraude.n2s3.features.builder.{ConnectionRef, N2S3, NeuronGroupRef}
+import fr.univ_lille.cristal.emeraude.n2s3.features.builder.{N2S3, NeuronGroupObserverRef, NeuronGroupRef}
 import fr.univ_lille.cristal.emeraude.n2s3.features.io.input.{SampleInput, SampleUnitInput, StreamSupport}
-import fr.univ_lille.cristal.emeraude.n2s3.features.io.report.BenchmarkMonitorRef
-import fr.univ_lille.cristal.emeraude.n2s3.models.qbg.{QBGNeuron, QBGNeuronConnectionWithNegative}
+import fr.univ_lille.cristal.emeraude.n2s3.features.logging.graph.SynapsesWeightGraphBuilderRef
+import fr.univ_lille.cristal.emeraude.n2s3.models.qbg.QBGNeuron
 import fr.univ_lille.cristal.emeraude.n2s3.support.io.{Input, InputSeq, InputStream, N2S3Input}
 
 import scala.collection.mutable
@@ -30,20 +31,22 @@ object NetworkImplicits {
     def using(connectionType: ConnectionPolicy): Unit = {
       network.getNeuronGroupById(originId)
         .addNeighbour(this.destinationId, connectionType)
+      network.getNeuronGroupRef(this.originId).connectTo(network.getNeuronGroupRef(this.destinationId), connectionType)
     }
   }
 
   implicit def stringToNeuronGroupRef(id: String)(implicit network: Network[SampleUnitInput, SampleInput]): NeuronGroupRef = {
-    network.getLayer(id)
+    network.getNeuronGroupRef(id)
   }
-
 
   implicit def stringToNeuronGroup(id: String)(implicit network: Network[SampleUnitInput, SampleInput]): Module = {
-    network.getNeuronGroupById(id)
+    try{
+      network.getNeuronGroupById(id)
+    }
+    catch {
+      case nse: NoSuchElementException => basicNeuronGroup(id)
+    }
   }
-
-
-
   def fixWeights(neuronGroupRef: NeuronGroupRef): Unit = {
     neuronGroupRef.fixNeurons()
   }
@@ -52,13 +55,9 @@ object NetworkImplicits {
     network.addBenchmarkObserver(id)
   }
 
-  implicit def getBenchmarkObserverOn(id: String)(implicit network: Network[SampleUnitInput, SampleInput]): BenchmarkMonitorRef = {
-    network.getBenchmarkObserver(id)
-  }
-
-  def makeSynapsesWeightGraph(originId: String, destinationId: String)(implicit network: Network[SampleUnitInput, SampleInput]): Unit ={
+  def observeConnectionsBetween(originId: String, destinationId: String)(implicit network: Network[SampleUnitInput, SampleInput]): SynapsesWeightGraphBuilderRef = {
     network.addSynapsesWeightGraph(originId, destinationId)
-
+    network.toN2S3.addNetworkObserver(new SynapsesWeightGraphBuilderRef(originId, destinationId))
   }
 
   class Module(identifier: String) {
@@ -66,7 +65,7 @@ object NetworkImplicits {
 
     val id: String = identifier
     private var neuronParameters: Seq[(Property[_], _)] = Seq()
-    private var neuronsCount = 1
+    var neuronsCount = 1
     private var neuronModel: NeuronModel = QBGNeuron
     private var neighbours: Seq[(String, ConnectionPolicy)] = Seq()
     private var isInput: Boolean = false
@@ -82,8 +81,9 @@ object NetworkImplicits {
       this
     }
 
-    def withParameters(properties: (Property[_], _)*): Unit = {
+    def withParameters(properties: (Property[_], _)*): Module = {
       this.neuronParameters = properties
+      this
     }
 
     def makeInput(): Module = {
@@ -95,111 +95,63 @@ object NetworkImplicits {
       this.neighbours = this.neighbours :+ (id, connection)
     }
 
-    def toNeuronGroup[U <: Input, T <: InputSeq[U]](n2S3: N2S3, input: StreamSupport[T, InputSeq[N2S3Input]], inputStream: InputStream[T]): (String, NeuronGroupRef) = {
-      if (this.isInput) {
-        val inputModule = n2S3.createInput(input)
-        input.append(inputStream)
-        (this.id, inputModule)
-      }
-      else {
-        (this.id, n2S3.createNeuronGroup(this.id, this.neuronsCount).setNeuronModel(this.neuronModel, this.neuronParameters))
-      }
+    def toInputNeuronGroup[U <: Input, T <: InputSeq[U]](n2s3: N2S3, input: StreamSupport[T, InputSeq[N2S3Input]]): (String, NeuronGroupRef) = {
+      val inputModule = n2s3.createInput(input)
+      (this.id, inputModule)
     }
 
-    def connect(neuronGroups: collection.mutable.Map[String, NeuronGroupRef]) {
-      def lookUpAndConnect(param: (String, ConnectionPolicy)): ConnectionRef = {
-        neuronGroups(this.id).connectTo(neuronGroups(param._1), param._2)
-      }
 
-      this.neighbours.foreach(lookUpAndConnect)
+    def toNeuronGroup(n2s3: N2S3): (String, NeuronGroupRef) = {
+      (this.id, n2s3.createNeuronGroup(this.id, this.neuronsCount).setNeuronModel(this.neuronModel, this.neuronParameters))
     }
   }
 
   class Network[U <: Input, T <: InputSeq[U]]() {
 
 
+    private val n2s3 = new N2S3
+
     private var modules: Map[String, Module] = Map()
     private var benchmarkedModules: mutable.MutableList[String] = new mutable.MutableList[String]()
     private var graphedConnections: mutable.MutableList[(String, String)] = new mutable.MutableList[(String, String)]()
-    private var originModuleToBeLinked: Option[String] = None
-    private var secondModuleToBeLinked: Option[String] = None
-    private var connectionToBeUsed: ConnectionPolicy = new FullConnection
-    private var input: Option[StreamSupport[T, InputSeq[N2S3Input]]] = None
-    private var inputStream: Option[InputStream[T]] = None
-    private var lastModule: Option[String] = None
+    private var inputStream: Option[StreamSupport[T, InputSeq[N2S3Input]]] = None
     private val neuronGroups: collection.mutable.Map[String, NeuronGroupRef] = collection.mutable.Map()
-    private val activeBenchmarkObservers: collection.mutable.Map[String, BenchmarkMonitorRef] = collection.mutable.Map()
+    private val activeObservers: collection.mutable.Map[String, NeuronGroupObserverRef] = collection.mutable.Map()
 
-    def has(module: Module): Network[U, T] = {
-      addModule(module)
-      this
-    }
 
-    private def addModule(module: Module) = {
+    def addInputModule(module: Module): Module = {
       this.modules += (module.id -> module)
-      this.lastModule = Some(module.id)
+      val neuronGroup = module.toInputNeuronGroup[U, T](n2s3, this.inputStream.get)
+      this.neuronGroups.put(neuronGroup._1, neuronGroup._2)
       module
     }
 
-    def withNeuronGroup(id: String): Module = this.addModule(basicNeuronGroup(id))
-
-    def withInputNeuronGroup(id: String): Module = this.addModule(inputNeuronGroup(id))
-
-    def hasLayer(module: Module): Network[U, T] = {
+    def withNeuronGroup(module: Module): Unit = {
       this.modules += (module.id -> module)
-      this.modules(this.lastModule.get).addNeighbour(module.id, new FullConnection(() => new QBGNeuronConnectionWithNegative()))
-      this.lastModule = Some(module.id)
+      val neuronGroup = module.toNeuronGroup(n2s3)
+      this.neuronGroups.put(neuronGroup._1, neuronGroup._2)
+    }
+
+    def withInputNeuronGroup(id: String): Module = this.addInputModule(inputNeuronGroup(id))
+
+    def hasInput(input: StreamSupport[T, InputSeq[N2S3Input]]): Network[U, T] = {
+      this.inputStream = Some(input)
       this
     }
 
-    def links(id: String): Network[U, T] = {
-      this.originModuleToBeLinked = Some(id)
-      this
-    }
-
-    def to(id: String): Network[U, T] = {
-      this.secondModuleToBeLinked = Some(id)
-      this.connectTemporaries()
-      this
-    }
-
-    def connectTemporaries(): Unit = {
-      this.modules(this.originModuleToBeLinked.get).addNeighbour(this.secondModuleToBeLinked.get, this.connectionToBeUsed)
-      this.clearTemporaries()
-    }
-
-    def clearTemporaries(): Unit = {
-      this.originModuleToBeLinked = None
-      this.secondModuleToBeLinked = None
-      this.connectionToBeUsed = new FullConnection
-    }
-
-    def hasInput(input: StreamSupport[T, InputSeq[N2S3Input]], inputStream: InputStream[T]): Network[U, T] = {
-      this.input = Some(input)
-      this.inputStream = Some(inputStream)
-      this
-    }
-
-    implicit def toN2S3: N2S3 = {
-      val n2s3 = new N2S3
-
-      def putToMap(module: Module) {
-        val res = module.toNeuronGroup[U, T](n2s3, this.input.get, this.inputStream.get)
-        this.neuronGroups.put(res._1, res._2)
-      }
-
-      this.modules.values.foreach(putToMap)
-      this.modules.values.foreach(_.connect(this.neuronGroups))
+    implicit def toN2S3: N2S3 = n2s3
+    /**
+    this.modules.values.foreach(_.connect(this.neuronGroups))
       this.benchmarkedModules.foreach(o => this.activeBenchmarkObservers.put(o, n2s3.createBenchmarkMonitor(this.getLayer(o))))
       this.graphedConnections.foreach(pair => n2s3.createSynapseWeightGraphOn(this.getLayer(pair._1), this.getLayer(pair._2)))
-      n2s3
-    }
+
+      */
 
     def getNeuronGroupById(id: String): Module = {
       this.modules(id)
     }
 
-    def getLayer(ident: String): NeuronGroupRef = {
+    def getNeuronGroupRef(ident: String): NeuronGroupRef = {
       this.neuronGroups(ident)
     }
 
@@ -211,8 +163,29 @@ object NetworkImplicits {
       this.benchmarkedModules += id
     }
 
-    def getBenchmarkObserver(id: String): BenchmarkMonitorRef = {
-      this.activeBenchmarkObservers(id)
+    def addObserver[V <: NeuronGroupObserverRef](ref: V): V = n2s3.addNetworkObserver(ref)
+
+    //    def getBenchmarkObserver(id: String): BenchmarkMonitorRef = {
+    //    this.activeBenchmarkObservers(id)
+    //}
+
+    def trainOn(stream : InputStream[T]): Unit ={
+      val networkStream = this.inputStream.get
+      networkStream.clean()
+      networkStream.append(stream)
+      this.n2s3.runAndWait()
+    }
+
+    def testOn(stream : InputStream[T]): Unit ={
+      val networkStream = this.inputStream.get
+      networkStream.clean()
+      networkStream.append(stream)
+      this.n2s3.layers.foreach(G => G.fixNeurons())
+      this.n2s3.runAndWait()
+    }
+
+    def getObserverOn(s: String): NeuronGroupObserverRef = {
+      this.activeObservers(s)
     }
   }
 
